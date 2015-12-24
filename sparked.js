fd4ea072b421490dc229fbc34ab947b94cff4097
Cyrus Roshan <CyrusRoshan@users.noreply.github.com>
2015-12-24 14:54:33 -0600

    made some changes

--- sparked.js
+++ sparked.js
@@ -2,7 +2,45 @@
 var inquirer = require("inquirer");
 var Slack = require('node-slack');
 var localtunnel = require('localtunnel');
+var serialPort = require("serialport");
+var Promise = require("bluebird");
 
+
+var serialPorts;
+
+serialPort.list(function (err, ports) {
+	serialPorts = ports;
+});
+
+inquirer.prompt([
+	{
+		type: "list",
+		name: "port",
+		message: "Which serial port is the arduino connected to?",
+		choices: serialPorts
+	},
+	{
+		type: "list",
+		name: "baud",
+		message: "What baud rate should serial begin at?",
+		choices: [
+			"115200",
+			"57600",
+			"38400",
+			"28800",
+			"19200",
+			"14400",
+			"9600",
+			"4800",
+			"2400",
+			"1200",
+			"600",
+			"300",
+		]
+	}
+], function(answers) {
+	console.log(answers);
+});
 
 var questions = [
 	{
@@ -75,23 +113,6 @@ var questions = [
 	}
 ];
 
-var questionsAfterSlack = [
-	{
-		type: "input",
-		name: "token",
-		message: "Add a outgoing webhook in slack to send notifications to the URL: " + tunnel.url + " and set 'dosparked' as the trigger word. Then, enter the slack token for outgoing payload validation",
-		default: 8080,
-		validate: function( value ) {
-			return true;
-			if (String(value).length === 24) {
-				return true;
-			} else {
-				return "Please enter a valid token";
-			}
-		},
-		when: getValue("slackAuth")
-	}
-]
 
 function getValue(key) {
 	return function(answers){
@@ -106,7 +127,23 @@ inquirer.prompt( questions, function(answers) {
 				console.error("Error connecting to localtunnel...")
 			}
 			else{
-				inquirer.prompt( questionsAfterSlack, function(answersAfterSlack) {
+				inquirer.prompt([
+					{
+						type: "input",
+						name: "token",
+						message: "Add a outgoing webhook in slack to send notifications to the URL: " + tunnel.url + " and set 'dosparked' as the trigger word. Then, enter the slack token for outgoing payload validation",
+						default: 8080,
+						validate: function( value ) {
+							return true;
+							if (String(value).length === 24) {
+								return true;
+							} else {
+								return "Please enter a valid token";
+							}
+						},
+						when: getValue("slackAuth")
+					}
+				], function(answersAfterSlack) {
 					answers += answersAfterSlack;
 					console.log( JSON.stringify(answers, null, "  ") );
 				});
