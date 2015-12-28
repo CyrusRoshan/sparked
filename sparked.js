74193884df926c864c91197032ca313509cc8f90
Cyrus Roshan <CyrusRoshan@users.noreply.github.com>
2015-12-28 03:42:03 -0600

    feature: added a few commands

--- sparked.js
+++ sparked.js
@@ -25,13 +25,17 @@ serialPort.listAsync().then(ports => ports)
     };
     if (answers.slackToken) {
         return new Promise(resolve => {
+
+            console.log('\n\nYour sparkedbot is being created!\nType "dosparked help" in slack to get a list of commands in slack\n');
+
             var Botkit = require('botkit');
 
             data.sparkedbot = Botkit.slackbot({
-                debug: false
+                debug: false,
+                log: false
             });
 
-            sparkedbot.spawn({
+            data.sparkedbot.spawn({
                 token: data.answers.slackToken,
             }).startRTM(err => {
                 if (err) {
@@ -47,11 +51,126 @@ serialPort.listAsync().then(ports => ports)
 
     .then(data => {
     if (data.sparkedbot) {
-        console.log('\n\n Your sparkedbot has been created!\nType "dosparked help" to get a list of commands in slack\n');
+        data.sparkedbot.hears(['status', 'current'], 'direct_message, direct_mention, mention', (bot, message) => {
+            var attachments = [];
+            var attachment = {
+                title: 'Status:',
+                color: '#FDD835',
+                fields: [],
+            }
 
-        data.sparkedbot.hears(['hello','hi'],'direct_message,direct_mention,mention',function(bot,message) {
+            attachment.fields.push({
+                label: 'Field',
+                value: 'Current port: ' + data.answers.port,
+                short: false,
+            })
+
+            attachment.fields.push({
+                label: 'Field',
+                value: 'Current baud rate: ' + data.answers.baud,
+                short: false,
+            })
+
+            attachment.fields.push({
+                label: 'Field',
+                value: 'File being monitored: ' + data.answers.filepath,
+                short: false,
+            })
+
+            attachments.push(attachment);
+
+            bot.reply(message, {
+                text: '',
+                attachments: attachments,
+            }, (err,resp) => {
+                //do nothing for now
+            });
+        });
+
+        data.sparkedbot.hears(['refetch', 'reupload', 'upload', 'update'], 'direct_message, direct_mention, mention', (bot, message) => {
+            bot.reply(message, 'Refetching and uploading ' + data.answers.filepath);
+            upload(data.answers.filepath);
+        });
+
+        data.sparkedbot.hears(['baud', 'rebaud'], 'direct_message, direct_mention, mention', (bot, message) => {
+            var baud = message.text.split(' ')[1];
+            var baudrates = getQuestions()[1].choices;
+
+            if (baudrates.indexOf(baud) != -1) {
+                bot.reply(message, 'Setting baud rate to ' + baud + '. Use "update" to finalize changes to settings');
+                data.answers.baud = baud;
+            } else {
+                bot.reply(message, baud + ' is an invalid baud rate, the following baud rates are supported:');
+                bot.reply(message, baudrates.join(', '));
+            }
+        });
+        data.sparkedbot.hears(['list', 'ports'], 'direct_message, direct_mention, mention', (bot, message) => {
+            serialPort.listAsync().then(ports => ports)
+                .then(ports => {
+
+                var attachments = [];
+                var attachment = {
+                    title: 'Available Ports:',
+                    color: '#FDD835',
+                    fields: [],
+                }
+
+                ports.forEach(port => {
+                    attachment.fields.push({
+                        label: 'Field',
+                        value: port.comName,
+                        short: false,
+                    })
+                });
+
+                attachments.push(attachment);
+
+                bot.reply(message, {
+                    text: '',
+                    attachments: attachments,
+                }, (err,resp) => {
+                    //do nothing for now
+                });
+            })
+        });
+        data.sparkedbot.hears(['change device', 'change port', 'switch port'], 'direct_message, direct_mention, mention', (bot, message) => {
+            var desiredPort = message.text.split(' ')[1];
+            var found = false;
+            serialPort.listAsync().then(ports => ports)
+                .then(ports => {
+                for (port in ports) {
+                    if(port.comName === desiredPort){
+                        found = true;
+                        break;
+                    }
+                };
+                if (found === false) {
+                    bot.reply(message, 'Sorry, but no serial port exists with the name ' + desiredPort);
+                } else {
+                    data.answers.port = desiredPort;
+                    bot.reply(message, desiredPort + ' is now the desired port. Use "update" to finalize changes to settings');
+                }
+            })
             bot.reply(message,"Hello.");
-        })
+        });
+        data.sparkedbot.hears(['change file'], 'direct_message, direct_mention, mention', (bot, message) => {
+            bot.reply(message,"Hello.");
+        });
+        data.sparkedbot.hears(['reauth'], 'direct_message, direct_mention, mention', (bot, message) => {
+            bot.reply(message,"Hello.");
+        });
+        data.sparkedbot.hears(['serialprint'], 'direct_message, direct_mention, mention', (bot, message) => {
+            bot.reply(message,"Hello.");
+        });
+        data.sparkedbot.hears(['talk here'], 'direct_message, direct_mention, mention', (bot, message) => {
+            bot.reply(message,"Hello.");
+        });
+        data.sparkedbot.hears(['stop talking'], 'direct_message, direct_mention, mention', (bot, message) => {
+            bot.reply(message,"Hello.");
+        });
+        data.sparkedbot.hears(['quit'], 'direct_message, direct_mention, mention', (bot, message) => {
+            bot.reply(message,"Hello.");
+        });
     }
     return data;
 })
@@ -63,7 +182,9 @@ serialPort.listAsync().then(ports => ports)
 
 
 function getQuestions(ports) {
-    var portNames = ports.map(port => port.comName);
+    if(ports){
+        var portNames = ports.map(port => port.comName);
+    }
 
     function getValue(value) {
         return answers => answers[ value ];
